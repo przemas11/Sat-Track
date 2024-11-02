81d37252e691e8f60b81d0a9726d163f2fafbbd0
przemas11 <26902854+przemas11@users.noreply.github.com>
2024-11-02 21:33:23 +0100

    Update dist

--- dist/Workers/createVectorTilePoints.js
+++ dist/Workers/createVectorTilePoints.js
@@ -1,1 +1,23 @@
-define(["./when-54c2dc71","./Check-6c0211bc","./Math-fc8cecf5","./Cartesian2-bddc1162","./AttributeCompression-9fc99391","./createTaskProcessorWorker"],function(a,e,C,g,b,r){"use strict";var w=32767,k=new g.Cartographic,v=new g.Cartesian3,y=new g.Rectangle,A=new g.Ellipsoid,M={min:void 0,max:void 0};return r(function(a,e){var r=new Uint16Array(a.positions);!function(a){a=new Float64Array(a);var e=0;M.min=a[e++],M.max=a[e++],g.Rectangle.unpack(a,2,y),e+=g.Rectangle.packedLength,g.Ellipsoid.unpack(a,e,A)}(a.packedBuffer);var t=y,n=A,i=M.min,s=M.max,c=r.length/3,o=r.subarray(0,c),u=r.subarray(c,2*c),p=r.subarray(2*c,3*c);b.AttributeCompression.zigZagDeltaDecode(o,u,p);for(var f=new Float64Array(r.length),h=0;h<c;++h){var l=o[h],d=u[h],m=p[h],l=C.CesiumMath.lerp(t.west,t.east,l/w),d=C.CesiumMath.lerp(t.south,t.north,d/w),m=C.CesiumMath.lerp(i,s,m/w),m=g.Cartographic.fromRadians(l,d,m,k),m=n.cartographicToCartesian(m,v);g.Cartesian3.pack(m,f,3*h)}return e.push(f.buffer),{positions:f.buffer}})});
+/**
+ * Cesium - https://github.com/AnalyticalGraphicsInc/cesium
+ *
+ * Copyright 2011-2017 Cesium Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Columbus View (Pat. Pend.)
+ *
+ * Portions licensed separately.
+ * See https://github.com/AnalyticalGraphicsInc/cesium/blob/master/LICENSE.md for full licensing details.
+ */
+define(["./defined-2a4f2d00","./Check-e5651467","./freezeObject-a51e076f","./defaultValue-29c9b1af","./Math-7782f09e","./Cartesian2-ba70b51f","./defineProperties-c817531e","./when-1faa3867","./AttributeCompression-5601f533","./createTaskProcessorWorker"],function(e,a,r,t,v,y,n,i,A,s){"use strict";var M=32767,R=new y.Cartographic,x=new y.Cartesian3,z=new y.Rectangle,D=new y.Ellipsoid,E={min:void 0,max:void 0};return s(function(e,a){var r=new Uint16Array(e.positions);!function(e){e=new Float64Array(e);var a=0;E.min=e[a++],E.max=e[a++],y.Rectangle.unpack(e,a,z),a+=y.Rectangle.packedLength,y.Ellipsoid.unpack(e,a,D)}(e.packedBuffer);var t=z,n=D,i=E.min,s=E.max,o=r.length/3,c=r.subarray(0,o),f=r.subarray(o,2*o),u=r.subarray(2*o,3*o);A.AttributeCompression.zigZagDeltaDecode(c,f,u);for(var p=new Float64Array(r.length),l=0;l<o;++l){var h=c[l],d=f[l],m=u[l],C=v.CesiumMath.lerp(t.west,t.east,h/M),b=v.CesiumMath.lerp(t.south,t.north,d/M),g=v.CesiumMath.lerp(i,s,m/M),w=y.Cartographic.fromRadians(C,b,g,R),k=n.cartographicToCartesian(w,x);y.Cartesian3.pack(k,p,3*l)}return a.push(p.buffer),{positions:p.buffer}})});
